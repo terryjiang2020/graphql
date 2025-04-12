6512b33bc6eed69b4eb0e1637c43fd12c6084b19
TerryAtRocketVentures <68800619+terryjiang2020@users.noreply.github.com>
2025-04-12 12:34:12 +1200

    try again:

--- examples/todo/main.go
+++ examples/todo/main.go
@@ -36,8 +36,8 @@ func main() {
 		result := executeQuery(r.URL.Query().Get("query"), schema.TodoSchema)
 		json.NewEncoder(w).Encode(result)
 	})
-
 	
+
 	// Serve static files
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
